e4a772925f9e6e3bc1a6f1d5fea8bef1202647d6
Gunses1987 <62013389+Gunses1987@users.noreply.github.com>
2021-09-26 18:02:10 +0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+console.log("main.js connected")
+
+
+
+var nav =document.querySelector('.nav')
+console.log(nav)
+var icon =document.querySelector('#icon')
+var closeMenu =document.querySelector('#close')
+
+
+icon.addEventListener('click',() => {
+    console.log("clicked")
+    nav.classList.add('open',)
+    icon.style.display='none'
+    closeMenu.style.display='initial'
+    
+});
+
+
+closeMenu.addEventListener('click',() => {
+    console.log("clicked2")
+    nav.classList.remove('open',)
+    icon.style.display='initial'
+    closeMenu.style.display='none'
+    
+});
+
+
+console.log("main.js connected")
+console.log("main.js connected")
