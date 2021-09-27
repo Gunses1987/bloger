dcc1348f75dbfc5d0bfc8ab8375dccb2e36a965e
Gunses1987 <62013389+Gunses1987@users.noreply.github.com>
2021-09-27 18:06:40 +0400

    doneee

--- js/main.js
+++ js/main.js
@@ -40,8 +40,37 @@ moveUp.addEventListener('click', ()=> {
 
 
 
+var b1 =document.querySelector('#b1')
+var b2 =document.querySelector('#b2')
+var b3 =document.querySelector('#b3')
+var b4 =document.querySelector('#b4')
 
+console.log(b1, b2, b3, b4)
 
+var rightArr =document.querySelector('#right')
+var leftArr =document.querySelector('#left')
+
+rightArr.addEventListener('click',() => {
+    console.log("clicked3")
+    b3.classList.remove('display-none')
+    b4.classList.remove('display-none')
+    b1.classList.add('display-none')
+    b2.classList.add('display-none')
+    rightArr.style.color='gray'
+    leftArr.style.color='black'
+    
+});
+
+leftArr.addEventListener('click',() => {
+    console.log("clicked3")
+    b1.classList.remove('display-none')
+    b2.classList.remove('display-none')
+    b3.classList.add('display-none')
+    b4.classList.add('display-none')
+    leftArr.style.color='gray'
+    rightArr.style.color='black'
+    
+});
 
 
 
